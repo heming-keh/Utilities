24b772c8ba43b2ae43b299209d12d836f7489b37
= <heming.keh@gmail.com>
2018-11-23 20:44:15 +0800

    termux ubuntu

---
+++ setupubuntu.sh
@@ -0,0 +1,62 @@
+# bash setupubuntu.sh
+# requirements: wget tar proot
+
+
+# getprop ro.product.cpu.abi
+ARCH="arm64"
+NAME="ubuntu"
+
+ROOTFS_URL="https://partner-images.canonical.com/core/bionic/current/ubuntu-bionic-core-cloudimg-${ARCH}-root.tar.gz"
+SHA256SUM_URL="${ROOTFS_URL%/*}/SHA256SUMS"
+ROOTFS_FILE=${ROOTFS_URL##*/}
+LINUX_ROOT="${HOME}/${NAME}"
+
+
+# create ${LINUX_ROOT}
+if [ -d ${LINUX_ROOT} ]; then
+  while true; do
+    read -p "${NAME} already exists, do you want to delete it? " yn
+    case ${yn} in
+      [Yy]* )
+        for entry in ${LINUX_ROOT}/*; do
+          if [[ $entry != *${ROOTFS_FILE} ]]; then
+            rm -fr $entry
+          fi
+        done
+        break;;
+      [Nn]* ) exit;;
+      * ) echo "please answer yes or no.";;
+    esac
+  done
+fi
+
+mkdir -p ${LINUX_ROOT}
+cd ${LINUX_ROOT}
+
+
+# fetch and extract ROOTFS
+if [ ! -e ${ROOTFS_FILE} ]; then
+  wget ${ROOTFS_URL}
+  wget ${SHA256SUM_URL}
+  sha256sum -c ${SHA256SUM_URL} || {
+    exit
+  }
+fi
+proot --link2symlink tar -xf ${ROOTFS_FILE} --exclude=./dev
+
+
+# create startup script
+mkdir -p ${HOME}/bin
+cat > ${HOME}/bin/${NAME} <<- EOM
+getprop | sed -n -e 's/^\[net\.dns.\]: \[\(.*\)\]/\1/p' | sed '/^\s*$/d' | sed 's/^/nameserver /' > ${LINUX_ROOT}/etc/resolv.conf
+unset LD_PRELOAD
+proot --link2symlink -0 -r ${LINUX_ROOT} -b /dev/ -b /proc/ -b /sdcard -b ${HOME}:/root -w /root /usr/bin/env HOME=/root TERM="${TERM}" LANG=${LANG} PATH=/bin:/usr/bin:/sbin:/usr/sbin /bin/sh --login
+EOM
+chmod a+x ${HOME}/bin/${NAME}
+
+
+# repositories
+sed -i -e "s/archive\.ubuntu\.com/mirrors\.aliyun\.com/" ${LINUX}/etc/apt/sources.list
+
+
+echo "done"
