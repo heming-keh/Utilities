e8626fcf876a21faf2af95589447c31f8ccafb51
= <heming.keh@gmail.com>
2018-11-23 22:40:38 +0800

    termux refactor

--- termux/setupalpine.sh
+++ termux/setupalpine.sh
@@ -2,24 +2,34 @@
 # requirements: wget tar proot
 
 
+################################################################################
+# settings
+################################################################################
+
 VERSION="3.8"
-MINOR_VERSION="1"
+VERSION_MINOR="1"
 # getprop ro.product.cpu.abi
 ARCH="aarch64"
+NAME="alpine"
 
-MINROOTFS_URL="https://mirrors.aliyun.com/alpine/v${VERSION}/releases/${ARCH}/alpine-minirootfs-${VERSION}.${MINOR_VERSION}-${ARCH}.tar.gz"
-MINROOTFS_FILE=${MINROOTFS_URL##*/}
-ALPINE_ROOT="${HOME}/alpine"
+ROOTFS_URL="https://mirrors.aliyun.com/alpine/v${VERSION}/releases/${ARCH}/alpine-minirootfs-${VERSION}.${VERSION_MINOR}-${ARCH}.tar.gz"
+SHA256SUM_URL="${ROOTFS_URL}.sha256"
+ROOTFS_FILE=${ROOTFS_URL##*/}
+SHA256SUM_FILE=${SHA256SUM_URL##*/}
+LINUX_ROOT="${HOME}/.${NAME}"
 
 
-# create ${ALPINE_ROOT}
-if [ -d ${ALPINE_ROOT} ]; then
+################################################################################
+# create ${LINUX_ROOT}
+################################################################################
+
+if [ -d ${LINUX_ROOT} ]; then
   while true; do
     read -p "apline already exists, do you want to delete it? " yn
     case ${yn} in
       [Yy]* )
-        for entry in ${ALPINE_ROOT}/*; do
-          if [[ $entry != *${MINROOTFS_FILE} ]]; then
+        for entry in ${LINUX_ROOT}/*; do
+          if [[ $entry != *${ROOTFS_FILE} ]] && [[ $entry != *${SHA256SUM_FILE} ]]; then
             rm -fr $entry
           fi
         done
@@ -30,36 +40,52 @@ if [ -d ${ALPINE_ROOT} ]; then
   done
 fi
 
-mkdir -p ${ALPINE_ROOT}
-cd ${ALPINE_ROOT}
+mkdir -p ${LINUX_ROOT}
+cd ${LINUX_ROOT}
 
 
+################################################################################
 # fetch and extract ROOTFS
-if [ ! -e ${MINROOTFS_FILE} ]; then
-  wget ${MINROOTFS_URL}
-  wget ${MINROOTFS_URL}.sha256
-  sha256sum -c ${MINROOTFS_FILE}.sha256 || {
-    exit
-  }
+################################################################################
+
+if [ ! -e ${ROOTFS_FILE} ]; then
+  wget ${ROOTFS_URL}
 fi
-proot --link2symlink tar -xf ${MINROOTFS_FILE} --exclude=./dev
+
+if [ ! -e ${SHA256SUM_FILE} ]; then
+  wget ${SHA256SUM_URL}
+fi
+
+sha256sum -c ${SHA256SUM_FILE} || {
+  exit
+}
+
+echo "extracting ..."
+proot --link2symlink tar -xf ${ROOTFS_FILE} --exclude=./dev
 
 
+################################################################################
 # create alpine startup script
+################################################################################
+
 mkdir -p ${HOME}/bin
-cat > ${HOME}/bin/alpine <<- EOM
-ALPINE_ROOT="${HOME}/alpine"
-getprop | sed -n -e 's/^\[net\.dns.\]: \[\(.*\)\]/\1/p' | sed '/^\s*$/d' | sed 's/^/nameserver /' > ${ALPINE_ROOT}/etc/resolv.conf
+cat > ${HOME}/bin/${NAME} <<- EOM
+getprop | sed -n -e 's/^\[net\.dns.\]: \[\(.*\)\]/\1/p' | sed '/^\s*$/d' | sed 's/^/nameserver /' > ${LINUX_ROOT}/etc/resolv.conf
 unset LD_PRELOAD
-proot --link2symlink -0 -r ${ALPINE_ROOT} -b /dev/ -b /proc/ -b /sdcard -b ${HOME}:/root -w /root /usr/bin/env HOME=/root TERM="${TERM}" LANG=${LANG} PATH=/bin:/usr/bin:/sbin:/usr/sbin /bin/sh --login
+proot --link2symlink -0 -r ${LINUX_ROOT} -b /dev/ -b /proc/ -b /sdcard -b ${HOME}:/root -w /root /usr/bin/env HOME=/root TERM="${TERM}" LANG=${LANG} PATH=/bin:/usr/bin:/sbin:/usr/sbin /bin/sh --login
 EOM
-chmod a+x ${HOME}/bin/alpine
+chmod a+x ${HOME}/bin/${NAME}
 
 
+################################################################################
 # repositories
-sed -i -e "s/^/#/" ${ALPINE_ROOT}/etc/apk/repositories
-echo "http://mirrors.aliyun.com/alpine/v${VERSION}/main" >> ${ALPINE_ROOT}/etc/apk/repositories
-echo "http://mirrors.aliyun.com/alpine/v${VERSION}/community" >> ${ALPINE_ROOT}/etc/apk/repositories
+################################################################################
+
+sed -i -e "s/^/#/" ${LINUX_ROOT}/etc/apk/repositories
+echo "http://mirrors.aliyun.com/alpine/v${VERSION}/main" >> ${LINUX_ROOT}/etc/apk/repositories
+echo "http://mirrors.aliyun.com/alpine/v${VERSION}/community" >> ${LINUX_ROOT}/etc/apk/repositories
 
 
-echo "done"
+echo
+echo "Make sure ~/bin in your PATH, use command ${NAME} to start."
+
--- termux/setupubuntu.sh
+++ termux/setupubuntu.sh
@@ -2,7 +2,12 @@
 # requirements: wget tar proot
 
 
+################################################################################
+# settings
+################################################################################
+
 # getprop ro.product.cpu.abi
+# aarch64 is arm64
 ARCH="arm64"
 NAME="ubuntu"
 
@@ -10,10 +15,13 @@ ROOTFS_URL="https://partner-images.canonical.com/core/bionic/current/ubuntu-bion
 SHA256SUM_URL="${ROOTFS_URL%/*}/SHA256SUMS"
 ROOTFS_FILE=${ROOTFS_URL##*/}
 SHA256SUM_FILE=${SHA256SUM_URL##*/}
-LINUX_ROOT="${HOME}/${NAME}"
+LINUX_ROOT="${HOME}/.${NAME}"
 
 
+################################################################################
 # create ${LINUX_ROOT}
+################################################################################
+
 if [ -d ${LINUX_ROOT} ]; then
   while true; do
     read -p "${NAME} already exists, do you want to delete it? " yn
@@ -35,7 +43,10 @@ mkdir -p ${LINUX_ROOT}
 cd ${LINUX_ROOT}
 
 
+################################################################################
 # fetch and extract ROOTFS
+################################################################################
+
 if [ ! -e ${ROOTFS_FILE} ]; then
   wget ${ROOTFS_URL}
 fi
@@ -48,21 +59,30 @@ sed -n /${ARCH}/p ${SHA256SUM_FILE} | sha256sum -c || {
   exit
 }
 
-proot --link2symlink tar -xvf ${ROOTFS_FILE} --exclude=./dev --exclude=dev
+echo "extracting ..."
+proot --link2symlink tar -xf ${ROOTFS_FILE} --exclude=./dev --exclude=dev
 
 
+################################################################################
 # create startup script
+################################################################################
+
 mkdir -p ${HOME}/bin
 cat > ${HOME}/bin/${NAME} <<- EOM
 getprop | sed -n -e 's/^\[net\.dns.\]: \[\(.*\)\]/\1/p' | sed '/^\s*$/d' | sed 's/^/nameserver /' > ${LINUX_ROOT}/etc/resolv.conf
 unset LD_PRELOAD
-proot --link2symlink -0 -r ${LINUX_ROOT} -b /dev/ -b /proc/ -b /sdcard -b ${HOME}:/root -w /root /usr/bin/env -i HOME=/root TERM="${TERM}" LANG=${LANG} PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games /bin/bash --login
+proot --link2symlink -0 -r ${LINUX_ROOT} -b /dev/ -b /proc/ -b /sdcard -b ${HOME}:/root -w /root /usr/bin/env HOME=/root TERM="${TERM}" LANG=${LANG} PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games /bin/bash --login
 EOM
 chmod a+x ${HOME}/bin/${NAME}
 
 
-# repositories
+################################################################################
+# apt mirror
+################################################################################
+
 sed -i -e "s/ports\.ubuntu\.com/mirrors\.aliyun\.com/" ${LINUX_ROOT}/etc/apt/sources.list
 
 
-echo "done"
+echo
+echo "Make sure ~/bin in your PATH, use command ${NAME} to start."
+
