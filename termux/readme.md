e06af000eac3574544808dfcaaba493ab108f469
= <heming.keh@gmail.com>
2018-11-26 21:03:42 +0800

    configuration ubuntu

--- termux/readme.md
+++ termux/readme.md
@@ -4,6 +4,29 @@
 2. [wiki](https://wiki.termux.com/wiki/Main_Page)
 3. [github](https://github.com/termux)
 
-# Swift
+# ubuntu
 
-[swift-arm](https://packagecloud.io/swift-arm/debian)
+## reinstall to include man pages
+
+    dpkg -l | grep ^ii | cut -d' ' -f3 | xargs apt-get install -y --reinstall && \
+    rm -r /var/lib/apt/lists/*
+
+## vim
+
+    git clone --depth 1 --recursive --shallow-submodules \
+    https://github.com/heming-keh/vim-pathogen.git ~/.vim
+
+## python
+
+scipy和numpy等一些软件包涉及底层硬件相关的一些编译，所以只能通过apt来安装。
+
+    apt install --no-install-recommends python3 python3-scipy python3-numpy python3-matplotlib
+    pip install setuptools wheel
+    pip install ipython sphinx
+
+
+## texlive
+
+    apt install --no-install-recommends texlive texlive-xelatex texlive-lang-cjk texlive-lang-chinese
+    apt install --no-install-recommends graphviz
+
--- termux/setupubuntu.sh
+++ termux/setupubuntu.sh
@@ -28,6 +28,7 @@ if [ -d ${LINUX_ROOT} ]; then
     read -p "${NAME} already exists, do you want to delete it? " yn
     case ${yn} in
       [Yy]* )
+        echo "deleting ..."
         for entry in ${LINUX_ROOT}/*; do
           if [[ $entry != *${ROOTFS_FILE} ]] && [[ $entry != *${SHA256SUM_FILE} ]]; then
             rm -fr $entry
@@ -81,10 +82,28 @@ chmod a+x ${HOME}/bin/${NAME}
 # configurations
 ################################################################################
 
-# mirror
+# apt mirror
 sed -i -e "s/ports\.ubuntu\.com/mirrors\.aliyun\.com/" ${LINUX_ROOT}/etc/apt/sources.list
-# vimrc
-echo "source /termux/tools/vimrc/_vimrc" > ${LINUX_ROOT}/root/.vimrc
+
+# include doc
+sed -i -e 's/\(path-exclude=\/usr\/share\/man\/\*\)/# \1/' ${LINUX_ROOT}/etc/dpkg/dpkg.cfg.d/excludes
+
+# pip mirror
+mkdir -p ${HOME}/.pip
+cat > ${HOME}/.pip/pip.conf <<- EOM
+[global]
+index-url = http://mirrors.aliyun.com/pypi/simple/
+
+[install]
+trusted-host=mirrors.aliyun.com
+EOM
+
+# aliases
+cat > ${HOME}/.bash_aliases <<- EOM
+alias python=python3
+alias pip=pip4
+EOM
+
 
 echo
 echo "Make sure ~/bin in your PATH, use command ${NAME} to start."
